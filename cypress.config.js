de0541ca12c4854b54a45ac67fe3f1a6cbb15fc3
dmytro.borysenko <dmytro.borysenko@appflame.com>
2026-06-21 15:29:47 +0300

    Fix cypress configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  allowCypressEnv: false,
+  screenshotsOnRunFailure: false,
+  video: false,
+  viewportWidth: 1280,
+  viewportHeight: 720,
   e2e: {
     specPattern: 'cypress/e2e/**/*.cy.js',
     supportFile: false,
-    screenshotsOnRunFailure: true,
-    video: true,
-    viewportWidth: 1280,
-    viewportHeight: 720,
   },
 })
